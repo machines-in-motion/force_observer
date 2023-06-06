fixed 1d model & derivatives !

ContactModel.py
@@ -3,7 +3,7 @@ DAM contact 6d with delta_f
 '''
 import numpy as np
 np.set_printoptions(precision=6, linewidth=180, suppress=True)
-np.random.seed(1)
+# np.random.seed(1)
 
 import crocoddyl
 import pinocchio as pin
@@ -13,7 +13,6 @@ class DAMRigidContact(crocoddyl.DifferentialActionModelAbstract):
     Computes the forward dynamics under rigid contact model + force estimate
     '''
     def __init__(self, stateMultibody, actuationModel, contactModelSum, costModelSum, frameId, delta_f):
-        # super(DAMSoftContactDynamics, self).__init__(stateMultibody, actuationModel.nu, costModelSum.nr)
         crocoddyl.DifferentialActionModelAbstract.__init__(self, stateMultibody, actuationModel.nu, costModelSum.nr)
         self.frameId = frameId
         self.with_armature = False
@@ -29,9 +28,16 @@ class DAMRigidContact(crocoddyl.DifferentialActionModelAbstract):
 
 
         self.nc = contactModelSum.nc
-        self.nv = 7
-        
-        assert(len(self.delta_f) == self.nc)
+        self.nv = self.pinocchio.nv
+        self.nq = self.pinocchio.nq
+        self.nx = self.nv + self.nq
+        self.ndx = stateMultibody.ndx
+
+        if(self.nc == 1):
+            self.J3d = np.zeros((3, self.pinocchio.nv))
+            assert(len(self.delta_f) == 3)
+        else:
+            assert(len(self.delta_f) == self.nc)
 
     def createData(self):
         '''
@@ -53,28 +59,38 @@ class DAMRigidContact(crocoddyl.DifferentialActionModelAbstract):
 
 
         if u is None:
-            print("hey")
             u = np.zeros(self.nu)
 
         # Actuation calc
         self.actuation.calc(data.multibody.actuation, x, u)
         self.contacts.calc(data.multibody.contacts, x)
 
-        # Add delta_f
-        new_tau = data.multibody.actuation.tau + data.multibody.contacts.Jc[:self.nc].T @ self.delta_f[:self.nc]
-
-        pin.forwardDynamics(self.pinocchio, data.pinocchio,
-                                            new_tau, #u,
-                                            data.multibody.contacts.Jc[:self.nc],
-                                            data.multibody.contacts.a0[:self.nc],
-                                            0.)
+        # Add delta_f in torque space
+        if(self.nc == 1):
+            self.J3d = pin.getFrameJacobian(self.pinocchio, data.pinocchio, self.frameId, pin.LOCAL)[:3]
+            new_tau = data.multibody.actuation.tau + self.J3d.T @ self.delta_f # Need 3D jac 
+            pin.forwardDynamics(self.pinocchio, data.pinocchio,
+                                                new_tau, #u,
+                                                data.multibody.contacts.Jc.reshape((1, self.nv)), # 1D
+                                                data.multibody.contacts.a0, # 1D
+                                                0.)
+        else:
+            new_tau = data.multibody.actuation.tau + data.multibody.contacts.Jc[:self.nc].T @ self.delta_f[:self.nc] 
+            pin.forwardDynamics(self.pinocchio, data.pinocchio,
+                                                new_tau, #u,
+                                                data.multibody.contacts.Jc[:self.nc], 
+                                                data.multibody.contacts.a0[:self.nc], 
+                                                0.)
         data.xout = data.pinocchio.ddq
-        self.contacts.updateAcceleration(data.multibody.contacts, data.pinocchio.ddq)
+        self.contacts.updateAcceleration(data.multibody.contacts, data.pinocchio.ddq) 
         # Here we compute the cost without delta f
-        self.contacts.updateForce(data.multibody.contacts, data.pinocchio.lambda_c)# - self.delta_f) 
+        self.contacts.updateForce(data.multibody.contacts, data.pinocchio.lambda_c)# - self.delta_f)  # 1D with lambda_c
         self.costs.calc(data.costs, x, u)
         # Here we add again delta_f to the computed force for dynamics
-        self.contacts.updateForce(data.multibody.contacts, data.pinocchio.lambda_c + self.delta_f)
+        if(self.nc == 1):
+            data.multibody.contacts.fext[self.parentId] += self.jMf.act(pin.Force(np.concatenate([self.delta_f, np.zeros(3)])))
+        else:
+            self.contacts.updateForce(data.multibody.contacts, data.pinocchio.lambda_c + self.delta_f)    # 3D with (0,0,lambda_c) + delta_f
         data.cost = data.costs.cost
         # pin.updateGlobalPlacements(self.pinocchio, data.pinocchio)
         return data.xout, data.cost
@@ -95,28 +111,35 @@ class DAMRigidContact(crocoddyl.DifferentialActionModelAbstract):
         # Actuation calcDiff
         self.actuation.calcDiff(data.multibody.actuation, x, u)
 
-        pin.computeRNEADerivatives(self.pinocchio, data.pinocchio, q, v, data.xout, data.multibody.contacts.fext)
-        data.Kinv = pin.getKKTContactDynamicMatrixInverse(self.pinocchio, data.pinocchio, data.multibody.contacts.Jc[:self.nc])
+        pin.computeRNEADerivatives(self.pinocchio, data.pinocchio, q, v, data.xout, data.multibody.contacts.fext) # 3D derivative with fext including delta_f
+        if(self.nc == 1):
+            data.Kinv = pin.getKKTContactDynamicMatrixInverse(self.pinocchio, data.pinocchio, data.multibody.contacts.Jc.reshape((1, self.nv))) # Kinv has nc=1 
+        else:
+            data.Kinv = pin.getKKTContactDynamicMatrixInverse(self.pinocchio, data.pinocchio, data.multibody.contacts.Jc[:self.nc])  
 
         self.actuation.calcDiff(data.multibody.actuation, x, u)
-        self.contacts.calcDiff(data.multibody.contacts, x)
+        self.contacts.calcDiff(data.multibody.contacts, x) 
 
         a_partial_dtau = data.Kinv[:self.nv, :self.nv]
         a_partial_da = data.Kinv[:self.nv, -self.nc:]
         f_partial_dtau = data.Kinv[-self.nc:, :self.nv]
         f_partial_da = data.Kinv[-self.nc:, -self.nc:]
 
-        data.Fx[:,:self.nv]  = -a_partial_dtau @ data.pinocchio.dtau_dq ##
+        data.Fx[:,:self.nv]  = -a_partial_dtau @ data.pinocchio.dtau_dq 
         data.Fx[:,-self.nv:] = -a_partial_dtau @ data.pinocchio.dtau_dv
-        # import pdb; pdb.set_trace()
-        data.Fx -= a_partial_da @ data.multibody.contacts.da0_dx[:self.nc]
+        if(self.nc == 1):
+            data.Fx -= a_partial_da @ data.multibody.contacts.da0_dx.reshape((1,self.ndx)) # 1D
+        else:
+            data.Fx -= a_partial_da @ data.multibody.contacts.da0_dx[:self.nc]
         data.Fx += a_partial_dtau @ data.multibody.actuation.dtau_dx
-        # import pdb; pdb.set_trace()
         data.Fu = a_partial_dtau @ data.multibody.actuation.dtau_du
 
-        data.df_dx[:self.nc, :self.nv] = f_partial_dtau @ data.pinocchio.dtau_dq ##
+        data.df_dx[:self.nc, :self.nv] = f_partial_dtau @ data.pinocchio.dtau_dq 
         data.df_dx[:self.nc, -self.nv:] = f_partial_dtau @ data.pinocchio.dtau_dv
-        data.df_dx[:self.nc,:] += f_partial_da @ data.multibody.contacts.da0_dx[:self.nc]
+        if(self.nc == 1):
+            data.df_dx[:self.nc,:] += f_partial_da @ data.multibody.contacts.da0_dx.reshape((1,self.ndx)) 
+        else:
+            data.df_dx[:self.nc,:] += f_partial_da @ data.multibody.contacts.da0_dx[:self.nc]
         data.df_dx[:self.nc,:] -= f_partial_dtau @ data.multibody.actuation.dtau_dx
         data.df_du[:self.nc,:] = -f_partial_dtau @ data.multibody.actuation.dtau_du
         # self.contacts.updateAccelerationDiff(data.multibody.contacts, data.Fx[-self.nv:])

test_contact.py
@@ -1,11 +1,12 @@
 import pinocchio as pin
 import example_robot_data as robex
 import numpy as np
-from numpy.linalg import norm
-from numpy.random import rand 
 np.set_printoptions(precision=4, linewidth=180)
 import crocoddyl
 from ContactModel import DAMRigidContact
+import sobec
+
+
 
 np.random.seed(10)
 
@@ -16,35 +17,34 @@ TOL = 1e-3
 robot_name = 'talos_arm'# 'kinova'  #'talos_arm'
 contactFrameName = 'wrist_left_ft_tool_link'  #'j2s6s200_joint_finger_tip_1' # 'wrist_left_ft_tool_link'
 robot = robex.load(robot_name)
-model = robot.model
+robot.data = robot.model.createData()
 # Initial conditions
-q = pin.randomConfiguration(model) 
-v = rand(model.nv) 
-a = rand(model.nv) 
-tau = rand(model.nv) 
-x = np.concatenate([q, v])
+q0 = pin.randomConfiguration(robot.model) 
+v0 = np.random.rand(robot.model.nv) 
+a0 = np.random.rand(robot.model.nv) 
+tau0 = np.random.rand(robot.model.nv) 
+x0 = np.concatenate([q0, v0])
 # BG gains
 gains = np.zeros(2)
-nq = model.nq
-nv = model.nv
-nc = 6 #6
-
+nq = robot.model.nq
+nv = robot.model.nv
+nc = 3
 
 # Numerical difference function
-def numdiff(f,x0,h=1e-6):
-    f0 = f(x0).copy()
-    x = x0.copy()
+def numdiff(f,inX,h=1e-6):
+    f0 = f(inX).copy()
+    x = inX.copy()
     Fx = []
     for ix in range(len(x)):
         x[ix] += h
         Fx.append((f(x)-f0)/h)
-        x[ix] = x0[ix]
+        x[ix] = inX[ix]
     return np.array(Fx).T
 
 
 contact_frame_id = robot.model.getFrameId(contactFrameName)
-# pin.forwardKinematics(robot.model, robot.data, q, v, a)
-# pin.updateFramePlacements(robot.model, robot.data)
+pin.forwardKinematics(robot.model, robot.data, q0, v0, a0)
+pin.updateFramePlacements(robot.model, robot.data)
 
 
 state = crocoddyl.StateMultibody(robot.model)
@@ -53,50 +53,57 @@ actuation = crocoddyl.ActuationModelFull(state)
 runningCostModel = crocoddyl.CostModelSum(state)
 terminalCostModel = crocoddyl.CostModelSum(state)
 # Contact model 
-contactModel = crocoddyl.ContactModelMultiple(state, actuation.nu)
 # Create 3D contact on the en-effector frame
+contactModel = sobec.ContactModelMultiple(state, actuation.nu)
 # contact_position = robot.data.oMf[contact_frame_id].copy()
 baumgarte_gains  = np.array([0., 50.])
 if(nc == 3):
-    contactItem = crocoddyl.ContactModel3D(state, contact_frame_id, robot.data.oMf[contact_frame_id].translation, baumgarte_gains) 
-else:
-    contactItem = crocoddyl.ContactModel6D(state, contact_frame_id, robot.data.oMf[contact_frame_id], baumgarte_gains) 
+    contactItem = sobec.ContactModel3D(state, contact_frame_id, robot.data.oMf[contact_frame_id].translation, baumgarte_gains, pin.LOCAL) 
+elif(nc == 6):
+    contactItem = sobec.ContactModel6D(state, contact_frame_id, robot.data.oMf[contact_frame_id], baumgarte_gains, pin.LOCAL) 
+elif(nc == 1):
+    contactItem = sobec.ContactModel1D(state, contact_frame_id, robot.data.oMf[contact_frame_id].translation, actuation.nu, baumgarte_gains, sobec.Vector3MaskType.z, pin.LOCAL) 
+else: pass
 # Populate contact model with contacts
 contactModel.addContact("contact", contactItem, active=True)
 # Create cost terms 
 uResidual = crocoddyl.ResidualModelContactControlGrav(state)
 uRegCost = crocoddyl.CostModelResidual(state, uResidual)
-xResidual = crocoddyl.ResidualModelState(state, x)
+xResidual = crocoddyl.ResidualModelState(state, x0 )
 xRegCost = crocoddyl.CostModelResidual(state, xResidual)
 desired_wrench = np.array([0., 0., -100., 0., 0., 0.])
-frameForceResidual = crocoddyl.ResidualModelContactForce(state, contact_frame_id, pin.Force(desired_wrench), nc, actuation.nu)
+frameForceResidual = sobec.ResidualModelContactForce(state, contact_frame_id, pin.Force(desired_wrench), nc, actuation.nu)
 contactForceCost = crocoddyl.CostModelResidual(state, frameForceResidual)
 runningCostModel.addCost("stateReg", xRegCost, 1e-2)
 runningCostModel.addCost("ctrlRegGrav", uRegCost, 1e-4)
 runningCostModel.addCost("force", contactForceCost, 10.)
 terminalCostModel.addCost("stateReg", xRegCost, 1e-2)
 # Create Differential Action Model (DAM), i.e. continuous dynamics and cost functions
-delta_f = 100*np.random.rand(nc) # delta_f[2] = -0
+if(nc == 1):
+    delta_f = 100*np.random.rand(3) # delta_f[2] = -0
+else:
+    delta_f = 100*np.random.rand(nc) # delta_f[2] = -0
 DAM = DAMRigidContact(state, actuation, contactModel, runningCostModel, contact_frame_id, delta_f)
 DAD = DAM.createData()
 jMf = robot.model.frames[contact_frame_id].placement
 
 def ddq_dam(dam,dad, q, v, u):
-    x = np.concatenate([q,v])
-    dam.calc(dad, x, u)
+    dam.calc(dad, np.concatenate([q,v]), u)
     return dad.xout
 def df_dam(dam, dad, q, v, u):
-    x = np.concatenate([q,v])
-    dam.calc(dad, x, u)
+    dam.calc(dad, np.concatenate([q,v]), u)
     cd = dad.multibody.contacts.contacts['contact']
-    return jMf.actInv(cd.f).vector[:nc]
+    if(nc == 1):
+        return jMf.actInv(cd.f).vector[2]
+    else:
+        return jMf.actInv(cd.f).vector[:nc]
 def tau_dam(dam, dad, q, v, u):
     pin.rnea(dam.pinocchio, dad.pinocchio, q, v, dad.xout, dad.multibody.contacts.fext)
     return dad.pinocchio.tau 
 
 # relation between LOCAL and LWA derivatives
-DAM.calc(DAD, x, tau)
-DAM.calcDiff(DAD, x, tau)
+DAM.calc(DAD, x0, tau0)
+DAM.calcDiff(DAD, x0, tau0)
 
 # Joint acc derivatives
 ddq_dq = DAD.Fx[:,:nq]
@@ -111,24 +118,24 @@ dtau_dq = DAD.pinocchio.dtau_dq
 dtau_dv = DAD.pinocchio.dtau_dv
 
 # Joint acc derivatives (ND)
-ddq_dq_ND = numdiff(lambda q_:ddq_dam(DAM, DAD, q_, v, tau), q)
-ddq_dv_ND = numdiff(lambda v_:ddq_dam(DAM, DAD, q, v_, tau), v)
-ddq_dtau_ND = numdiff(lambda tau_:ddq_dam(DAM, DAD, q, v, tau_), tau)
+ddq_dq_ND = numdiff(lambda q_:ddq_dam(DAM, DAD, q_, v0, tau0), q0)
+ddq_dv_ND = numdiff(lambda v_:ddq_dam(DAM, DAD, q0, v_, tau0), v0)
+ddq_dtau_ND = numdiff(lambda tau_:ddq_dam(DAM, DAD, q0, v0, tau_), tau0)
 # Force derivative (ND)
-df_dq_ND = numdiff(lambda q_:df_dam(DAM, DAD, q_, v, tau), q)
-df_dv_ND = numdiff(lambda v_:df_dam(DAM, DAD, q, v_, tau), v)
-df_dtau_ND = numdiff(lambda tau_:df_dam(DAM, DAD, q, v, tau_), tau)
+df_dq_ND = numdiff(lambda q_:df_dam(DAM, DAD, q_, v0, tau0), q0)
+df_dv_ND = numdiff(lambda v_:df_dam(DAM, DAD, q0, v_, tau0), v0)
+df_dtau_ND = numdiff(lambda tau_:df_dam(DAM, DAD, q0, v0, tau_), tau0)
 # RNEA derivatives (ND)
-dtau_dq_ND = numdiff(lambda q_:tau_dam(DAM, DAD, q_, v, tau), q)
-dtau_dv_ND = numdiff(lambda v_:tau_dam(DAM, DAD, q, v_, tau), v)
+dtau_dq_ND = numdiff(lambda q_:tau_dam(DAM, DAD, q_, v0, tau0), q0)
+dtau_dv_ND = numdiff(lambda v_:tau_dam(DAM, DAD, q0, v_, tau0), v0)
 
 # Tests
-assert(norm(ddq_dq - ddq_dq_ND)<=TOL)
-assert(norm(ddq_dv - ddq_dv_ND)<=TOL)
-assert(norm(ddq_dtau - ddq_dtau_ND)<=TOL)
-assert(norm(df_dq - df_dq_ND)<=TOL)      
-assert(norm(df_dv - df_dv_ND)<=TOL)
-assert(norm(df_dtau - df_dtau_ND)<=TOL)
+assert(np.linalg.norm(ddq_dq - ddq_dq_ND)<=TOL)
+assert(np.linalg.norm(ddq_dv - ddq_dv_ND)<=TOL)
+assert(np.linalg.norm(ddq_dtau - ddq_dtau_ND)<=TOL)
+assert(np.linalg.norm(df_dq - df_dq_ND)<=TOL)      
+assert(np.linalg.norm(df_dv - df_dv_ND)<=TOL)
+assert(np.linalg.norm(df_dtau - df_dtau_ND)<=TOL)
 
-assert(norm(dtau_dq - dtau_dq_ND)<=TOL)   
-assert(norm(dtau_dv - dtau_dv_ND)<=TOL)   
+assert(np.linalg.norm(dtau_dq - dtau_dq_ND)<=TOL)   
+assert(np.linalg.norm(dtau_dv - dtau_dv_ND)<=TOL)   