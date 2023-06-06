fixed derivatives & unittest of contact6D

test_contact6d.py
@@ -71,7 +71,7 @@ runningCostModel.addCost("ctrlRegGrav", uRegCost, 1e-4)
 runningCostModel.addCost("force", contactForceCost, 10.)
 terminalCostModel.addCost("stateReg", xRegCost, 1e-2)
 # Create Differential Action Model (DAM), i.e. continuous dynamics and cost functions
-delta_f = 1000*np.ones(6) # delta_f[2] = -0
+delta_f = 100*np.random.rand(6) # delta_f[2] = -0
 DAM = DAMRigidContact6D(state, actuation, contactModel, runningCostModel, contact_frame_id, delta_f)
 DAD = DAM.createData()
 jMf = robot.model.frames[contact_frame_id].placement
@@ -86,9 +86,8 @@ def df_dam(dam, dad, q, v, u):
     cd = dad.multibody.contacts.contacts['contact']
     return jMf.actInv(cd.f).vector
 def tau_dam(dam, dad, q, v, u):
-    x = np.concatenate([q,v])
-    dam.calc(dad, x, u)
-    return dad.pinocchio.tau
+    pin.rnea(dam.pinocchio, dad.pinocchio, q, v, dad.xout, dad.multibody.contacts.fext)
+    return dad.pinocchio.tau 
 
 # relation between LOCAL and LWA derivatives
 DAM.calc(DAD, x, tau)
@@ -97,9 +96,11 @@ DAM.calcDiff(DAD, x, tau)
 # Joint acc derivatives
 ddq_dq = DAD.Fx[:,:nq]
 ddq_dv = DAD.Fx[:,nq:]
+ddq_dtau = DAD.Fu
 # Force derivatives
 df_dq = DAD.df_dx[:,:nq]
 df_dv = DAD.df_dx[:,nq:]
+df_dtau = DAD.df_du
 # RNEA derivatives
 dtau_dq = DAD.pinocchio.dtau_dq
 dtau_dv = DAD.pinocchio.dtau_dv
@@ -107,9 +108,11 @@ dtau_dv = DAD.pinocchio.dtau_dv
 # Joint acc derivatives (ND)
 ddq_dq_ND = numdiff(lambda q_:ddq_dam(DAM, DAD, q_, v, tau), q)
 ddq_dv_ND = numdiff(lambda v_:ddq_dam(DAM, DAD, q, v_, tau), v)
+ddq_dtau_ND = numdiff(lambda tau_:ddq_dam(DAM, DAD, q, v, tau_), tau)
 # Force derivative (ND)
 df_dq_ND = numdiff(lambda q_:df_dam(DAM, DAD, q_, v, tau), q)
 df_dv_ND = numdiff(lambda v_:df_dam(DAM, DAD, q, v_, tau), v)
+df_dtau_ND = numdiff(lambda tau_:df_dam(DAM, DAD, q, v, tau_), tau)
 # RNEA derivatives (ND)
 dtau_dq_ND = numdiff(lambda q_:tau_dam(DAM, DAD, q_, v, tau), q)
 dtau_dv_ND = numdiff(lambda v_:tau_dam(DAM, DAD, q, v_, tau), v)
@@ -117,7 +120,10 @@ dtau_dv_ND = numdiff(lambda v_:tau_dam(DAM, DAD, q, v_, tau), v)
 # Tests
 assert(norm(ddq_dq - ddq_dq_ND)<=TOL)
 assert(norm(ddq_dv - ddq_dv_ND)<=TOL)
-assert(norm(df_dq - df_dq_ND)<=TOL)        # FALSE
+assert(norm(ddq_dtau - ddq_dtau_ND)<=TOL)
+assert(norm(df_dq - df_dq_ND)<=TOL)      
 assert(norm(df_dv - df_dv_ND)<=TOL)
-# assert(norm(dtau_dq - dtau_dq_ND)<=TOL)    # FALSE 
-# assert(norm(dtau_dv - dtau_dv_ND)<=TOL)
+assert(norm(df_dtau - df_dtau_ND)<=TOL)
+
+assert(norm(dtau_dq - dtau_dq_ND)<=TOL)   
+assert(norm(dtau_dv - dtau_dv_ND)<=TOL)   