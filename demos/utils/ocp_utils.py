fixed bugs + code profiling + computed standard deviation + validated [delta_f > delta_tau] & [interior <=> exterior]

demos/utils/ocp_utils.py
@@ -22,10 +22,8 @@ logger = CustomLogger(__name__, GLOBAL_LOG_LEVEL, GLOBAL_LOG_FORMAT).logger
 
 
 # Check installed pkg
-USE_SOBEC = False 
 import sobec
-from ContactModel import DAMRigidContact
-from ContactModel1d_3d import DAMRigidContact1D3D
+from force_observer import DAMContactDeltaTau
 
 
 class OptimalControlProblemClassicalWithObserver(ocp.OptimalControlProblemClassical):
@@ -44,7 +42,7 @@ class OptimalControlProblemClassicalWithObserver(ocp.OptimalControlProblemClassi
     '''
     super().check_config()
 
-  def initialize(self, x0, delta_f, hybrid_df=False, pinRefFrame=pin.LOCAL, callbacks=False):
+  def initialize(self, x0, torque_offset=False, callbacks=False):
     '''
     Initializes OCP and FDDP solver from config parameters and initial state
       INPUT: 
@@ -60,59 +58,43 @@ class OptimalControlProblemClassicalWithObserver(ocp.OptimalControlProblemClassi
            a <--> weighted activation, with weights e.g. 'stateRegWeights' in config file 
            z <--> can be state x, control u, frame position or velocity, contact force, etc.
     ''' 
+    
   # State and actuation models
     state = crocoddyl.StateMultibody(self.rmodel)
     actuation = crocoddyl.ActuationModelFull(state)
   
-  # Make sure a contact model is defined
-    try: 
-       assert(hasattr(self, 'contacts'))
-    except: 
-      logger.error("The YANL file does not include any contact model !")
-
-    self.nb_contacts = len(self.contacts)
-    self.contact_types = [ct['contactModelType'] for ct in self.contacts]
-    logger.debug("Detected "+str(len(self.contacts))+" contacts with types = "+str(self.contact_types))
+  # Contact or not ?
+    if(not hasattr(self, 'contacts')):
+      self.nb_contacts = 0
+    else:
+      self.nb_contacts = len(self.contacts)
+      self.contact_types = [ct['contactModelType'] for ct in self.contacts]
+      logger.debug("Detected "+str(len(self.contacts))+" contacts with types = "+str(self.contact_types))
 
   # Create IAMs
     runningModels = []
     for i in range(self.N_h):  
       # Create DAM (Contact or FreeFwd)
         # Initialize contact model if necessary and create appropriate DAM
-        contactModels = []
-        for ct in self.contacts:
+        if(self.nb_contacts > 0):
+          contactModels = []
+          for ct in self.contacts:
             contactModels.append(self.create_contact_model(ct, state, actuation))
-
-        # Create DAMContactDyn      
-        if(USE_SOBEC):
-            contactModelSum = sobec.ContactModelMultiple(state, actuation.nu)
-            contactModelSum.addContact(self.contacts[0]['contactModelFrameName'], contactModels[0], active=self.contacts[0]['active'])
-            dam = sobec.DifferentialActionModelContactFwdDynamics(state, 
-                                                                    actuation, 
-                                                                    contactModelSum, 
-                                                                    crocoddyl.CostModelSum(state, nu=actuation.nu), 
-                                                                    inv_damping=0., 
-                                                                    enable_force=True)
+        
+        if torque_offset:
+            dam = DAMContactDeltaTau(state, 
+                                actuation, 
+                                crocoddyl.ContactModelMultiple(state, actuation.nu), 
+                                crocoddyl.CostModelSum(state, nu=actuation.nu), 
+                                inv_damping=0., 
+                                enable_force=True)
         else:
-            contactModelSum = sobec.ContactModelMultiple(state, actuation.nu)
-            contactModelSum.addContact(self.contacts[0]['contactModelFrameName'], contactModels[0], active=self.contacts[0]['active'])
-            
-            if hybrid_df:
-                dam = DAMRigidContact1D3D(state, 
-                                    actuation, 
-                                    contactModelSum, 
-                                    crocoddyl.CostModelSum(state, nu=actuation.nu), 
-                                    contactModels[0].id,
-                                    delta_f,
-                                    pinRefFrame=pinRefFrame)
-            else:
-                dam = DAMRigidContact(state, 
-                                    actuation, 
-                                    contactModelSum, 
-                                    crocoddyl.CostModelSum(state, nu=actuation.nu), 
-                                    contactModels[0].id,
-                                    delta_f,
-                                    pinRefFrame=pinRefFrame)
+            dam = sobec.DifferentialActionModelContactFwdDynamics(state, 
+                                actuation, 
+                                crocoddyl.ContactModelMultiple(state, actuation.nu), 
+                                crocoddyl.CostModelSum(state, nu=actuation.nu), 
+                                inv_damping=0., 
+                                enable_force=True)
       # Create IAM from DAM
         runningModels.append(crocoddyl.IntegratedActionModelEuler(dam, stepTime=self.dt))
         
@@ -123,41 +105,35 @@ class OptimalControlProblemClassicalWithObserver(ocp.OptimalControlProblemClassi
         # Add armature to current IAM
         runningModels[i].differential.armature = np.asarray(self.armature)
 
+      # Contact model
+        # Add contact model to current IAM
+        if(self.nb_contacts > 0):
+          for k,contactModel in enumerate(contactModels):
+            runningModels[i].differential.contacts.addContact(self.contacts[k]['contactModelFrameName'], contactModel, active=self.contacts[k]['active'])
+
+
   # Terminal DAM (Contact or FreeFwd)
     # If contact, initialize terminal contact model and create terminal DAMContactDyn
-    contactModels = []
-    for ct in self.contacts:
+    if(self.nb_contacts > 0):
+      contactModels = []
+      for ct in self.contacts:
         contactModels.append(self.create_contact_model(ct, state, actuation))
 
     # Create terminal DAMContactDyn
-    if(USE_SOBEC):
-        contactModelSum = sobec.ContactModelMultiple(state, actuation.nu)
-        contactModelSum.addContact(self.contacts[0]['contactModelFrameName'], contactModels[0], active=self.contacts[0]['active'])
-        dam_t = sobec.DifferentialActionModelContactFwdDynamics(state, 
-                                                                actuation, 
-                                                                contactModelSum, 
-                                                                crocoddyl.CostModelSum(state, nu=actuation.nu), 
-                                                                inv_damping=0., 
-                                                                enable_force=True)
+    if torque_offset:
+        dam_t = DAMContactDeltaTau(state, 
+                            actuation, 
+                            sobec.ContactModelMultiple(state, actuation.nu), 
+                            crocoddyl.CostModelSum(state, nu=actuation.nu), 
+                                inv_damping=0., 
+                                enable_force=True)
     else:
-        contactModelSum = sobec.ContactModelMultiple(state, actuation.nu)
-        contactModelSum.addContact(self.contacts[0]['contactModelFrameName'], contactModels[0], active=self.contacts[0]['active'])
-        if hybrid_df:
-            dam_t = DAMRigidContact1D3D(state, 
+        dam_t = sobec.DifferentialActionModelContactFwdDynamics(state, 
                                 actuation, 
-                                contactModelSum, 
+                                sobec.ContactModelMultiple(state, actuation.nu), 
                                 crocoddyl.CostModelSum(state, nu=actuation.nu), 
-                                contactModels[0].id,
-                                delta_f,
-                                pinRefFrame=pinRefFrame)
-        else:
-            dam_t = DAMRigidContact(state, 
-                                actuation, 
-                                contactModelSum, 
-                                crocoddyl.CostModelSum(state, nu=actuation.nu), 
-                                contactModels[0].id,
-                                delta_f,
-                                pinRefFrame=pinRefFrame)
+                                inv_damping=0., 
+                                enable_force=True)
         
   # Create terminal IAM from terminal DAM
     terminalModel = crocoddyl.IntegratedActionModelEuler( dam_t, stepTime=0. )
@@ -181,7 +157,7 @@ class OptimalControlProblemClassicalWithObserver(ocp.OptimalControlProblemClassi
     problem = crocoddyl.ShootingProblem(x0, runningModels, terminalModel)
   
   # Creating the DDP solver 
-    ddp = crocoddyl.SolverFDDP(problem)
+    ddp = crocoddyl.SolverGNMS(problem)
   
   # Callbacks
     if(callbacks):

kuka_dgh/plot_compare.py
@@ -39,15 +39,15 @@ print(controlled_joint_ids)
 # Create data Plotter
 s = SimpleDataPlotter()
 
-data_path = '/home/skleff/Desktop/delta_f_real_exp/sanding/integral_tuning/'
-label1 = 'best'
-label2 = 'new'
+data_path = '/home/skleff/Desktop/delta_f_real_exp/sanding/d_tau_vs_df/'
+label1 = 'iter=3'
+label2 = 'iter=4'
 # label3 = 'friction_comp+delta_f'
 
 SAVE = False
 
 print("Load data 1...")
-r1 = DataReader(data_path+'config_REAL_2023-08-30T12:12:31.109803_best.mds')  
+r1 = DataReader(data_path+'config_REAL_2023-09-08T16:54:39.495862_df_internal_iter=3.mds')  
 print("Load data 2...")
 r2 = DataReader(data_path+'.mds') 
 # print("Load data 3...")
@@ -64,6 +64,12 @@ from core_mpc import analysis_utils
 
 N = min(r1.data['tau'].shape[0], r2.data['tau'].shape[0])
 # N = min(N, r3.data['tau'].shape[0])
+
+plt.figure()
+plt.plot(r1.data['ddp_iter'], label=label1)
+plt.plot(r2.data['ddp_iter'], label=label2)
+plt.legend()
+# ax[0].plot(N*[1./config['plan_freq']], label= 'mpc')
 
 target_force_3d = np.zeros((N, 3))
 target_force_3d[:,-1] = config['frameForceRef'][2]
@@ -101,145 +107,146 @@ fig_f1, _ = s.plot_soft_contact_force([
                             'dotted'
                             ])
 
-
-fig_f2, _ = s.plot_soft_contact_force([
-                           np.abs(r1.data['contact_force_3d_measured'][N_START:N] - target_force_3d[N_START:N]), 
-                           np.abs(r2.data['contact_force_3d_measured'][N_START:N] - target_force_3d[N_START:N]), 
-                          #  np.abs(r3.data['contact_force_3d_measured'][N_START:N] - target_force_3d[N_START:N]), 
-                           target_force_3d[N_START:N] - target_force_3d[N_START:N]
-                           ],
-                           [
-                            label1,
-                            label2, 
-                            # label3,
-                            'ref'
-                          ], 
-                          [
-                            'b', 
-                            'g', 
-                            # 'r', 
-                            'k'
-                            ],
-                          linestyle=[
-                            'solid', 
-                            'solid', 
-                            # 'solid', 
-                            'dotted'
-                            ])
-
-
-
-p_mea1 = get_p_(r1.data['joint_positions'][N_START:N,controlled_joint_ids][:,controlled_joint_ids], pinrobot.model, pinrobot.model.getFrameId('contact'))
-p_mea2 = get_p_(r2.data['joint_positions'][N_START:N,controlled_joint_ids][:,controlled_joint_ids], pinrobot.model, pinrobot.model.getFrameId('contact'))
-# p_mea3 = get_p_(r3.data['joint_positions'][N_START:N,controlled_joint_ids][:,controlled_joint_ids], pinrobot.model, pinrobot.model.getFrameId('contact'))
-target_position = np.zeros((N-N_START, 3))
-target_position[:,0] = r1.data['target_position_x'][N_START:N,0]
-target_position[:,1] = r1.data['target_position_y'][N_START:N,0]
-target_position[:,2] = r1.data['target_position_z'][N_START:N,0]
-target_position2 = np.zeros((N-N_START, 3))
-target_position2[:,0] = r2.data['target_position_x'][N_START:N,0]
-target_position2[:,1] = r2.data['target_position_y'][N_START:N,0]
-target_position2[:,2] = r2.data['target_position_z'][N_START:N,0]
-# target_position3 = np.zeros((N-N_START, 3))
-# target_position3[:,0] = r3.data['target_position_x'][N_START:N,0]
-# target_position3[:,1] = r3.data['target_position_y'][N_START:N,0]
-# target_position3[:,2] = r3.data['target_position_z'][N_START:N,0]
-fig_p1, _ = s.plot_ee_pos( [
-                           p_mea1,
-                           p_mea2 ,
-                          #  p_mea3,
-                           target_position 
-                           ],
-                           [
-                            label1, 
-                            label2, 
-                            # label3, 
-                            'ref'
-                          ], 
-                          [
-                            'b', 
-                            'g', 
-                            'r', 
-                            'k'
-                            ],
-                          linestyle=[
-                            'solid', 
-                            'solid', 
-                            'solid', 
-                            'dotted'
-                            ])
-
-fig_p2, _ = s.plot_ee_pos( [
-                           np.abs(p_mea1 - target_position),
-                           np.abs(p_mea2 - target_position2),
-                          #  np.abs(p_mea3 - target_position3),
-                           target_position - target_position
-                           ],
-                           [
-                            label1, 
-                            label2,
-                            # label3, 
-                            'ref'
-                          ], 
-                          [
-                            'b', 
-                            'g', 
-                            'r', 
-                            'k'
-                            ],
-                          linestyle=[
-                            'solid', 
-                            'solid', 
-                            'solid', 
-                            'dotted'
-                            ])
-
-
-# s.plot_joint_tau([r1.data['tau'][:,controlled_joint_ids] + r1.data['tau_gravity'][:,controlled_joint_ids],
-#                    r2.data['tau'][:,controlled_joint_ids] + r2.data['tau_gravity'][:,controlled_joint_ids],
-#                    r3.data['tau'][:,controlled_joint_ids] + r3.data['tau_gravity'][:,controlled_joint_ids]],
-#               ['Desired (+g(q))'+label1, 'Desired (+g(q))'+label2, 'Desired (+g(q))'+label3], 
-#               [[0.,0.,0.,0.], 'b', 'g', 'r'],
-#               ylims=[-model.effortLimit, +model.effortLimit] )
-
-
-
-fig, ax = plt.subplots(4, 1, sharex='col') 
-ax[0].plot(r1.data['time_df'], label='time_df'+label1)
-ax[0].plot(r2.data['time_df'], label='time_df'+label2)
-# ax[0].plot(r3.data['time_df'], label='time_df'+label3)
-
-ax[1].plot(r1.data['t_child'], label='child'+label1)
-ax[1].plot(r2.data['t_child'], label='child'+label2)
-# ax[1].plot(r3.data['t_child'], label='child'+label3)
-
-ax[2].plot(r1.data['ddp_iter'], label='iter'+label1)
-ax[2].plot(r2.data['ddp_iter'], label='iter'+label2)
-# ax[2].plot(r3.data['ddp_iter'], label='iter'+label3)
-
-ax[3].plot(r1.data['t_run'], label='t_run'+label1)
-ax[3].plot(r2.data['t_run'], label='t_run'+label2)
-# ax[3].plot(r3.data['t_run'], label='t_run'+label3)
-ax[0].legend(); ax[1].legend(); ax[2].legend(); ax[3].legend()
-
-
-print("------------------------------------")
-print("------------------------------------")
-print(label1+" Pxy error norm = ", np.linalg.norm(p_mea1[N_START:N,:2] - target_position[N_START:N,:2]) )
-print(label2+" Pxy error norm = ", np.linalg.norm(p_mea2[N_START:N,:2] - target_position2[N_START:N,:2]))
-# print(label3+" Pxy error norm = ", np.linalg.norm(p_mea3[N_START:N,:2] - target_position3[N_START:N,:2]))
-print(label1+" Fz mean abs error = ", np.mean(np.abs(r1.data['contact_force_3d_measured'][N_START:N, 2] - target_force_3d[N_START:N,2])))
-print(label2+" Fz mean abs error = ", np.mean(np.abs(r2.data['contact_force_3d_measured'][N_START:N, 2] - target_force_3d[N_START:N,2])))
-# print(label3+" Fz mean abs error = ", np.mean(np.abs(r3.data['contact_force_3d_measured'][N_START:N, 2] - target_force_3d[N_START:N,2])))
-print("------------------------------------")
-print("------------------------------------")
-
-
-# if(SAVE):
-#   fig_f1.savefig(data_path+label1+'_vs_'+label2+'_vs_'+label3+'_compare_force.png')
-#   fig_f2.savefig(data_path+label1+'_vs_'+label2+'_vs_'+label3+'_compare_force_err.png')
-#   # fig_p1.savefig(data_path+label1+'_vs_'+label2+'_vs_'+label3'_compare_pos.png')
-#   fig_p2.savefig(data_path+label1+'_vs_'+label2+'_vs_'+label3+'_compare_pos_err.png')
-
-
 plt.show()
+
+# fig_f2, _ = s.plot_soft_contact_force([
+#                            np.abs(r1.data['contact_force_3d_measured'][N_START:N] - target_force_3d[N_START:N]), 
+#                            np.abs(r2.data['contact_force_3d_measured'][N_START:N] - target_force_3d[N_START:N]), 
+#                           #  np.abs(r3.data['contact_force_3d_measured'][N_START:N] - target_force_3d[N_START:N]), 
+#                            target_force_3d[N_START:N] - target_force_3d[N_START:N]
+#                            ],
+#                            [
+#                             label1,
+#                             label2, 
+#                             # label3,
+#                             'ref'
+#                           ], 
+#                           [
+#                             'b', 
+#                             'g', 
+#                             # 'r', 
+#                             'k'
+#                             ],
+#                           linestyle=[
+#                             'solid', 
+#                             'solid', 
+#                             # 'solid', 
+#                             'dotted'
+#                             ])
+
+
+
+# p_mea1 = get_p_(r1.data['joint_positions'][N_START:N,controlled_joint_ids][:,controlled_joint_ids], pinrobot.model, pinrobot.model.getFrameId('contact'))
+# p_mea2 = get_p_(r2.data['joint_positions'][N_START:N,controlled_joint_ids][:,controlled_joint_ids], pinrobot.model, pinrobot.model.getFrameId('contact'))
+# # p_mea3 = get_p_(r3.data['joint_positions'][N_START:N,controlled_joint_ids][:,controlled_joint_ids], pinrobot.model, pinrobot.model.getFrameId('contact'))
+# target_position = np.zeros((N-N_START, 3))
+# target_position[:,0] = r1.data['target_position_x'][N_START:N,0]
+# target_position[:,1] = r1.data['target_position_y'][N_START:N,0]
+# target_position[:,2] = r1.data['target_position_z'][N_START:N,0]
+# target_position2 = np.zeros((N-N_START, 3))
+# target_position2[:,0] = r2.data['target_position_x'][N_START:N,0]
+# target_position2[:,1] = r2.data['target_position_y'][N_START:N,0]
+# target_position2[:,2] = r2.data['target_position_z'][N_START:N,0]
+# # target_position3 = np.zeros((N-N_START, 3))
+# # target_position3[:,0] = r3.data['target_position_x'][N_START:N,0]
+# # target_position3[:,1] = r3.data['target_position_y'][N_START:N,0]
+# # target_position3[:,2] = r3.data['target_position_z'][N_START:N,0]
+# fig_p1, _ = s.plot_ee_pos( [
+#                            p_mea1,
+#                            p_mea2 ,
+#                           #  p_mea3,
+#                            target_position 
+#                            ],
+#                            [
+#                             label1, 
+#                             label2, 
+#                             # label3, 
+#                             'ref'
+#                           ], 
+#                           [
+#                             'b', 
+#                             'g', 
+#                             'r', 
+#                             'k'
+#                             ],
+#                           linestyle=[
+#                             'solid', 
+#                             'solid', 
+#                             'solid', 
+#                             'dotted'
+#                             ])
+
+# fig_p2, _ = s.plot_ee_pos( [
+#                            np.abs(p_mea1 - target_position),
+#                            np.abs(p_mea2 - target_position2),
+#                           #  np.abs(p_mea3 - target_position3),
+#                            target_position - target_position
+#                            ],
+#                            [
+#                             label1, 
+#                             label2,
+#                             # label3, 
+#                             'ref'
+#                           ], 
+#                           [
+#                             'b', 
+#                             'g', 
+#                             'r', 
+#                             'k'
+#                             ],
+#                           linestyle=[
+#                             'solid', 
+#                             'solid', 
+#                             'solid', 
+#                             'dotted'
+#                             ])
+
+
+# # s.plot_joint_tau([r1.data['tau'][:,controlled_joint_ids] + r1.data['tau_gravity'][:,controlled_joint_ids],
+# #                    r2.data['tau'][:,controlled_joint_ids] + r2.data['tau_gravity'][:,controlled_joint_ids],
+# #                    r3.data['tau'][:,controlled_joint_ids] + r3.data['tau_gravity'][:,controlled_joint_ids]],
+# #               ['Desired (+g(q))'+label1, 'Desired (+g(q))'+label2, 'Desired (+g(q))'+label3], 
+# #               [[0.,0.,0.,0.], 'b', 'g', 'r'],
+# #               ylims=[-model.effortLimit, +model.effortLimit] )
+
+
+
+# fig, ax = plt.subplots(4, 1, sharex='col') 
+# ax[0].plot(r1.data['time_df'], label='time_df'+label1)
+# ax[0].plot(r2.data['time_df'], label='time_df'+label2)
+# # ax[0].plot(r3.data['time_df'], label='time_df'+label3)
+
+# ax[1].plot(r1.data['t_child'], label='child'+label1)
+# ax[1].plot(r2.data['t_child'], label='child'+label2)
+# # ax[1].plot(r3.data['t_child'], label='child'+label3)
+
+# ax[2].plot(r1.data['ddp_iter'], label='iter'+label1)
+# ax[2].plot(r2.data['ddp_iter'], label='iter'+label2)
+# # ax[2].plot(r3.data['ddp_iter'], label='iter'+label3)
+
+# ax[3].plot(r1.data['t_run'], label='t_run'+label1)
+# ax[3].plot(r2.data['t_run'], label='t_run'+label2)
+# # ax[3].plot(r3.data['t_run'], label='t_run'+label3)
+# ax[0].legend(); ax[1].legend(); ax[2].legend(); ax[3].legend()
+
+
+# print("------------------------------------")
+# print("------------------------------------")
+# print(label1+" Pxy error norm = ", np.linalg.norm(p_mea1[N_START:N,:2] - target_position[N_START:N,:2]) )
+# print(label2+" Pxy error norm = ", np.linalg.norm(p_mea2[N_START:N,:2] - target_position2[N_START:N,:2]))
+# # print(label3+" Pxy error norm = ", np.linalg.norm(p_mea3[N_START:N,:2] - target_position3[N_START:N,:2]))
+# print(label1+" Fz mean abs error = ", np.mean(np.abs(r1.data['contact_force_3d_measured'][N_START:N, 2] - target_force_3d[N_START:N,2])))
+# print(label2+" Fz mean abs error = ", np.mean(np.abs(r2.data['contact_force_3d_measured'][N_START:N, 2] - target_force_3d[N_START:N,2])))
+# # print(label3+" Fz mean abs error = ", np.mean(np.abs(r3.data['contact_force_3d_measured'][N_START:N, 2] - target_force_3d[N_START:N,2])))
+# print("------------------------------------")
+# print("------------------------------------")
+
+
+# # if(SAVE):
+# #   fig_f1.savefig(data_path+label1+'_vs_'+label2+'_vs_'+label3+'_compare_force.png')
+# #   fig_f2.savefig(data_path+label1+'_vs_'+label2+'_vs_'+label3+'_compare_force_err.png')
+# #   # fig_p1.savefig(data_path+label1+'_vs_'+label2+'_vs_'+label3'_compare_pos.png')
+# #   fig_p2.savefig(data_path+label1+'_vs_'+label2+'_vs_'+label3+'_compare_pos_err.png')
+
+
+# plt.show()

kuka_dgh/plot_expe.py
@@ -58,11 +58,11 @@ print("N_start = ", N_START)
 
 if(SIM):
     data_path = '/home/skleff/Desktop/delta_f_real_exp/sanding/d_tau_vs_df/'
-    data_name = 'config_SIM_2023-09-06T18:23:22.648137_test.mds'
+    data_name = 'config_SIM_2023-09-08T11:55:35.037265_dtau_external.mds'
     
 else:
     data_path = '/home/skleff/Desktop/delta_f_real_exp/sanding/d_tau_vs_df/'
-    data_name = 'config_REAL_2023-09-07T16:59:07.835076_dtau_external.mds'
+    data_name = 'config_REAL_2023-09-08T18:05:34.056236_df_internal_2.mds'
     
 # data_path = '/home/skleff/Desktop/soft_contact_real_exp/paper+video_datasets/slow/'
 # data_name = 'reduced_soft_mpc_contact1d_REAL_2023-07-07T14:09:22.468998_slow_exp_2'
@@ -71,8 +71,8 @@ r = DataReader(data_path+data_name)
 N = r.data['tau'].shape[0]
 
 fig, ax = plt.subplots(4, 1, sharex='col') 
-ax[0].plot(r.data['t_child'], label='t_solve')
-ax[0].plot(N*[1./config['plan_freq']], label= 'mpc')
+ax[0].plot(r.data['KKT'], label='KKT residual')
+# ax[0].plot(N*[1./config['plan_freq']], label= 'mpc')
 ax[1].plot(r.data['time_df'], label='DF_time')
 ax[1].plot(N*[1./config['plan_freq']], label= 'mpc')
 ax[2].plot(r.data['ddp_iter'], label='ddp iter')
@@ -105,10 +105,10 @@ if(config['USE_DELTA_F']):
     plt.plot(np.array(r.data['delta_f']))
     plt.title("delta_f")
 
-if(config['USE_DELTA_TAU']):
-    plt.figure()
-    plt.plot(np.array(r.data['delta_tau']))
-    plt.title("delta_tau")
+# if(config['USE_DELTA_TAU']):
+#     s.plot_joint_tau( [r.data['delta_tau']], 
+#                       ['delta_tau'], 
+#                       ['r'])
 
 # For SIM robot only
 if(SIM):
@@ -180,7 +180,19 @@ fig_f, _ = s.plot_soft_contact_force([
 plt.figure()
 plt.plot(r.data['force_integral'])
 
-print(" Fz mean abs error = ", np.mean(np.abs(r.data['contact_force_3d_measured'][N_START:N, 2] - target[N_START:,2])))
+# Compute average tracking error for each circle
+CIRCLE_PERIOD_IN_CYCLES = int(2*np.pi/3.*1000)
+N_START = N_START
+N_CIRCLE = int((N-N_START)/CIRCLE_PERIOD_IN_CYCLES)
+N = N_START + CIRCLE_PERIOD_IN_CYCLES * N_CIRCLE
+
+error_traj = np.abs(r.data['contact_force_3d_measured'][N_START:, 2] - target[N_START:, 2])
+mean_errors = [np.mean(error_traj[t*CIRCLE_PERIOD_IN_CYCLES:(t+1)*CIRCLE_PERIOD_IN_CYCLES]) for t in range(N_CIRCLE)]
+print(mean_errors)
+
+print(" Fz mean abs error      = ", np.mean(np.abs(r.data['contact_force_3d_measured'][N_START:N, 2] - target[N_START:N,2])))
+print(" Fz mean abs error [1:] = ", np.mean(mean_errors[1:]), r'$\pm$', np.std(mean_errors[1:]))
+print(" Fz mean abs error      = ", np.mean(mean_errors), r'$\pm$', np.std(mean_errors))
 # print(" F3d mean abs error = ", np.mean(np.linalg.norm(np.abs(r.data['contact_force_3d_measured'][N_START:N, :] - target[N_START:, :]))))
 
 