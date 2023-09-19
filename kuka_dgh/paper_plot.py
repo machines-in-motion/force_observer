519b1c02ded76023c33d995fbea110568f9332f7
Sebastien Kleff <sk8001@nyu.edu>
2023-09-19 17:46:43 -0400

    dynamic plot running - task phases and pos smoothing missing

--- kuka_dgh/paper_plot.py
+++ kuka_dgh/paper_plot.py
@@ -1,105 +1,3 @@
-
-import matplotlib
-matplotlib.use('GTK3Agg') 
-
-
-from matplotlib.animation import FuncAnimation, FFMpegWriter
-import matplotlib.pyplot as plt
-# matplotlib.rcParams["pdf.fonttype"] = 42
-# matplotlib.rcParams["ps.fonttype"] = 42
-import numpy as np
-
-import time
-from matplotlib import pyplot as plt
-import numpy as np
-
-# plt.ion()
-
-# fig = plt.figure()
-# ax1 = fig.add_subplot(2, 1, 1)
-# ax2 = fig.add_subplot(2, 1, 2)
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex='col', figsize=(19.2,10.8))   
-fig.canvas.draw()   # note that the first draw comes before setting data 
-# cache the background
-axbackground = fig.canvas.copy_from_bbox(ax1.bbox)
-ax2background = fig.canvas.copy_from_bbox(ax2.bbox)
-# plt.show(block=False)
-
-
-# ax1.legend(framealpha=0.95, fontsize=26)     
-        
-ax1.grid(True) 
-ax2.grid(True) 
-ax1.set_xlim(0, 1)
-ax2.set_xlim(0, 1)
-ax1.set_ylim(-1.1, 1.1)
-ax2.set_ylim(-1.1, 1.1)
-ax1.set_ylabel('Force (N)', fontsize=26)
-ax2.set_ylabel('Position error (m)', fontsize=26)
-ax2.set_xlabel('Time (s)', fontsize=26)
-ax1.tick_params(axis = 'y', labelsize=22)
-ax2.tick_params(axis = 'x', labelsize=22)
-ax2.tick_params(axis = 'y', labelsize=22)
-ax1.tick_params(labelbottom=False)  
-# fig0.tight_layout()
-
-
-line1, = ax1.plot([], [], animated=True, label="hello")
-line2, = ax2.plot([], [], animated=True, label="hello")
-line = [line1, line2]
-
-N = 100
-
-x = np.linspace(0, 1, N)
-y1 = np.sin(x)
-y2 = np.cos(x)
-
-def init():
-    """
-    This init function defines the initial plot parameter
-    """
-    # Set initial parameter for the plot
-    return line
-
-def animate(t):
-    """
-    This function will be called periodically by FuncAnimation. Frame parameter will be passed on each call as a counter. 
-    """
-
-    line[0].set_data(x[:t], y1[:t])
-    line[1].set_data(x[:t], y2[:t])
-
-    return line
-
-# Create FuncAnimation object and plt.show() to show the updated animation
-
-import time
-
-t0 = time.time()
-
-Frames = [i for i in range(0, N)]
-ani = FuncAnimation(fig, animate, frames=Frames, repeat=False, interval = 10, init_func = init, blit=True)
-ani.save('animation.mp4')
-
-
-print("COMPUTE TIME = ", time.time() - t0)
-# plt.show()
-
-
-assert False
-
-
-
-
-
- 
-
-
-
-
-
-
-
 import sys
 sys.path.append("../../")
 from force_feedback_dgh.demos.utils.plot_utils import SimpleDataPlotter
@@ -164,9 +62,9 @@ data_path = '/home/skleff/Desktop/delta_f_real_exp/video/'
 
 
 print("Load data 1...")
-r1 = DataReader(data_path+'config_REAL_2023-09-19T12:17:31.957715_test.mds') 
+r1 = DataReader(data_path+'config_REAL_2023-09-19T12:17:31.957715_default.mds') 
 print("Load data 2...")
-r2 = r1
+r2 =  DataReader(data_path+'config_REAL_2023-09-19T17:21:16.680326_FL_DF.mds') 
 
 
 label1 = 'Default'
@@ -174,7 +72,7 @@ label2 = r'FL + $\Delta F$ (PM)'
 
 
 N = min(r1.data['tau'].shape[0], r2.data['tau'].shape[0])
-
+N = 25000
  
 print("N_start = ", N_START)
 
@@ -195,18 +93,11 @@ else:
     force_3d_1 = r1.data['contact_force_3d_measured'][N_START:N]
     force_3d_2 = r2.data['contact_force_3d_measured'][N_START:N] 
 
-from matplotlib.animation import FuncAnimation, FFMpegWriter
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.rcParams["pdf.fonttype"] = 42
-matplotlib.rcParams["ps.fonttype"] = 42
- 
-color_list = ['b', 'g', 'r', 'y']
 
 
 def compute_pos_error_traj(r):
-    p_mea = get_p_(r.data['joint_positions'][N_START:,controlled_joint_ids], pinrobot.model, pinrobot.model.getFrameId('contact'))
-    return np.sqrt((p_mea[:, 0] - r.data['target_position_x'][N_START:,0])**2 + (p_mea[:, 1] - r.data['target_position_y'][N_START:,1])**2)
+    p_mea = get_p_(r.data['joint_positions'][N_START:N,controlled_joint_ids], pinrobot.model, pinrobot.model.getFrameId('contact'))
+    return np.sqrt((p_mea[:, 0] - r.data['target_position_x'][N_START:N,0])**2 + (p_mea[:, 1] - r.data['target_position_y'][N_START:N,1])**2)
 
 
 pos_error_1 = np.zeros((N-N_START, 1))
@@ -229,18 +120,40 @@ time_lin = np.linspace(0, (N-N_START) / config['simu_freq'], (N-N_START))
 
 
 
+color_list = ['b', 'g', 'r', 'y']
 
-#Initiate figure and ax objects
 
-fig0, (ax1, ax2) = plt.subplots(2, 1, figsize=(20, 18))
 
+
+
+
+
+import matplotlib
+matplotlib.use('GTK3Agg') 
+
+
+from matplotlib.animation import FuncAnimation, FFMpegWriter
+import matplotlib.pyplot as plt
+# matplotlib.rcParams["pdf.fonttype"] = 42
+# matplotlib.rcParams["ps.fonttype"] = 42
+import numpy as np
+
+import time
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex='col', figsize=(20, 18))
+
+fig.canvas.draw() 
 
 ax1.plot(time_lin, target_force_3d[N_START:N,2], color='k', linewidth=4, linestyle='--', label='Reference', alpha=0.5) 
 ax1.grid(True) 
 ax2.grid(True) 
-ax2.set_ylim(0., 0.02)
+ax2.set_ylim(0., 0.025)
 ax1.set_xlim(time_lin[0], time_lin[-1])
-ax1.set_ylim(0., 100.)
+ax1.set_ylim(20., 80.)
 ax2.set_xlim(time_lin[0], time_lin[-1])
 ax1.legend(framealpha=0.95, fontsize=26)     
 ax1.set_ylabel('Force (N)', fontsize=26)
@@ -250,26 +163,28 @@ ax1.tick_params(axis = 'y', labelsize=22)
 ax2.tick_params(axis = 'x', labelsize=22)
 ax2.tick_params(axis = 'y', labelsize=22)
 ax1.tick_params(labelbottom=False)  
-fig0.tight_layout()
+fig.tight_layout()
 
 
-x, y = [], []
-line1, = ax1.plot([], [], 'bo')
-# line,  = ax1.plot(time_lin, force_3d_1[:, 2], color=color_list[0], linewidth=4, label=label1, alpha=0.8)
-# line,  = ax1.plot(time_lin, force_3d_2, color=color_list[1], linewidth=4, label=label2, alpha=0.8)
-# line,  = ax2.plot(time_lin, pos_error_1, color=color_list[0], linewidth=4, label=label1, alpha=0.8)
-# line,  = ax2.plot(time_lin, pos_error_2, color=color_list[1], linewidth=4, label=label2, alpha=0.8)
+line1a, = ax1.plot(time_lin[0:1], force_3d_1[0:1, 2], animated=True, color=color_list[0], linewidth=4, label=label1, alpha=0.8)
+line1b, = ax1.plot(time_lin[0:1], force_3d_2[0:1, 2], animated=True, color=color_list[2], linewidth=4, label=label2, alpha=0.8)
 
 
-# ax1.plot(time_lin[:, 2], force_3d_1[:,2], color=color_list[0], linewidth=4, label=label1, alpha=0.8)
-# ax1.plot(time_lin[:, 2], force_3d_2[:,2], color=color_list[1], linewidth=4, label=label2, alpha=0.8)
-# ax2.plot(time_lin[:, 2], pos_error_1[:2], color=color_list[0], linewidth=4, label=label1, alpha=0.8)
-# ax2.plot(time_lin[:, 2], pos_error_2[:2], color=color_list[1], linewidth=4, label=label2, alpha=0.8)
+line2a, = ax2.plot(time_lin[0:1], pos_error_1[0:1], animated=True, color=color_list[0], linewidth=4, label=label1, alpha=0.8)
+line2b, = ax2.plot(time_lin[0:1], pos_error_2[0:1], animated=True, color=color_list[2], linewidth=4, label=label2, alpha=0.8)
 
 
 
+line = [line1a, line1b, line2a, line2b]
+
+ax1.legend(loc="upper left", framealpha=0.95, fontsize=26) 
 
 
+T = int((N-N_START)/1000)
+PPS = 50  # Point per second
+
+N_FRAMES = int(T * PPS)
+SKIP = int(1000/PPS)
 
 
 
@@ -279,26 +194,36 @@ def init():
     This init function defines the initial plot parameter
     """
     # Set initial parameter for the plot
-    return line1,
+    return line
 
-def animate(frame):
+def animate(t):
     """
     This function will be called periodically by FuncAnimation. Frame parameter will be passed on each call as a counter. 
     """
-    # Append data to x and y data list
 
-    t = int(frame*1000)
-    
-    line1.set_data(time_lin[:t:100], force_3d_1[:t:100, 2])
-    return line1,
+    line[0].set_data(time_lin[0:t:SKIP], force_3d_1[0:t:SKIP, 2])
+    line[1].set_data(time_lin[0:t:SKIP], force_3d_2[0:t:SKIP, 2])
+
+
+    line[2].set_data(time_lin[0:t:SKIP], pos_error_1[0:t:SKIP])
+    line[3].set_data(time_lin[0:t:SKIP], pos_error_2[0:t:SKIP])
+
+
+    return line
 
 # Create FuncAnimation object and plt.show() to show the updated animation
-ani = FuncAnimation(fig0, animate, frames = time_lin, interval = 100, init_func = init)
+
+import time
+
+t0 = time.time()
+
+Frames = [i * SKIP for i in range(0, N_FRAMES)]
+ani = FuncAnimation(fig, animate, frames=Frames, repeat=False, interval = SKIP, init_func = init, blit=True)
 ani.save('animation.mp4')
 
-plt.show()
 
+print("COMPUTE TIME = ", time.time() - t0)
+# plt.show()
 
-assert False
 
 
